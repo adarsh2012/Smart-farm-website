b51b0d1878a4dfd60e96c9fb7185d3ed528e1b6f
adarsh2012 <tadarsh2012@gmail.com>
2020-03-14 14:34:34 +0700

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <AddTodo />
+    <Todo
+      v-bind:todoList="todos"
+      v-on:del-todo="deleteTodo"
+      @addItem="addItem"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todo from "./components/Todo.vue";
+import AddTodo from "./components/AddTodo.vue";
+import Header from "./components/layout/header";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Todo,
+    Header,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          name: "Adarsh",
+          home: "House",
+          complete: true
+        },
+        {
+          id: 2,
+          name: "Bradarsh",
+          home: "Road",
+          complete: false
+        }
+      ]
+    };
+  },
+  methods: {
+    deleteTodo: function(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addItem: function() {
+      var newData = {
+        id: Math.random(),
+        name: "mia" + Math.round(10 * Math.random(), 3),
+        home: "Khalifa",
+        complete: false
+      };
+      this.todos.push(newData);
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -21,7 +61,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
